d70b976824f0d4e834ec7262481664c064ba37ee
Gerald Bauer <gerald.bauer@gmail.com>
2013-08-18 14:14:44 +0200

    update engine

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121004210237) do
+ActiveRecord::Schema.define(:version => 1) do
 
   create_table "cities", :force => true do |t|
     t.string   "title",                         :null => false
